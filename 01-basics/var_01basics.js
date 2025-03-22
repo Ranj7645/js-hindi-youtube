dcd745006aac08840a18f52404db6aa39dd88fae
Ranjeet kumar <167242355+Ranj7645@users.noreply.github.com>
2025-03-22 11:49:18 +0000

    succesfully

---
+++ 01-basics/var_01basics.js
@@ -0,0 +1,15 @@
+const bankAccountNo=12345
+
+let bankName="HDFC"
+var bankId=12345
+var a
+let b
+
+bankManager="Shyam"
+// bankAccountNo=11123
+console.log(bankAccountNo)
+console.table([bankAccountNo ,bankName ,bankId ,bankManager,a,b])
+
+/*
+    caution! -- Prefer not to use var as key words due to conflicts at functional scope and variable scope
+*/
