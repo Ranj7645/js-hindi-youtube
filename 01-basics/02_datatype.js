a8a179dd4044000033dd9dfa4eb07d21b16ca34d
Ranjeet kumar <167242355+Ranj7645@users.noreply.github.com>
2025-03-23 08:30:05 +0000

    complete the commit.

---
+++ 01-basics/02_datatype.js
@@ -0,0 +1,19 @@
+"use strict" // Treat all js code as newer version
+
+//  alert(3+3) // we are using node js , not browser
+
+console.log(typeof "Hitesh")
+// Data types :-
+// number (range from 2^53)
+// bigint
+// string 
+// boolean
+// null
+// undefined
+// symbol
+
+console.log(typeof undefined); //undefined(these are data types of js)
+console.log(typeof null);//object(these are data types of js)
+
+
+
